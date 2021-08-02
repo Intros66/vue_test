98b9883b79fdca59a2161001392a03bfb75a7db0
Intros66 <small_wn@163.com>
2021-08-02 17:42:32 +0800

    分析脚手架

---
+++ 01_src_分析脚手架/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+      <img src="./assets/logo.png" alt="vue-logo">
+      <School></School>
+      <Student></Student>
+  </div>
+</template>
+
+<script>
+    //引入组件
+    import School from './components/School'
+    import Student from './components/Student'
+
+    export default {
+        name: 'App',
+        components: {
+            School,
+            Student
+        }
+    }
+</script>
+
+<style>
+
+</style>
+
---
+++ 01_src_分析脚手架/components/Student.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- 组件的结构 -->
+  <div>
+    <h2>学生名称：{{ name }}</h2>
+    <h2>年龄：{{ address }}</h2>
+  </div>
+</template>
+
+<script>
+    /* 
+        export xxx
+        export {xxx}
+        export default xxx
+    */
+    // 组件交互的代码（数据、方法等等）
+    export default {
+    name: "student",
+    data() {
+        return {
+        name: "张三",
+        address: 18
+        };
+    },
+
+    };
+</script>
