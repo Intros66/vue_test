10d5e46aa159212f40386d48673fdae3bf505cdd
Intros66 <small_wn@163.com>
2021-08-03 09:47:28 +0800

    ref属性

---
+++ 02_src_ref属性/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+      <h1 v-text="msg" ref="hName"></h1>
+      <button @click="showDOM">点我输出上方的DOM元素</button>
+      <School ref="s"></School>
+  </div>
+</template>
+
+<script>
+//引入School组件
+import School from './components/School'
+
+export default {
+    name: 'App',
+    data() {
+        return {
+            msg: '欢迎'
+        }
+    },
+     components: {
+        School
+    },
+    methods: {
+        showDOM(){
+            console.log(this.$refs);
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ 02_src_ref属性/components/School.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="school">
+    <h2>学生姓名：{{ name }}</h2>
+    <h2>学生性别：{{ gender }}</h2>
+  </div>
+</template>
+
+<script>
+    export default {
+    name: "School",
+    data() {
+        return {
+        name: "张三",
+        gender: "男"
+        };
+    }
+  };
+</script>
+
