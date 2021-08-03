296560b741fa256bab58791233ec21c1262ea729
Intros66 <small_wn@163.com>
2021-08-03 09:34:32 +0800

    props

--- 03_src_props配置/components/Student.vue
+++ 03_src_props配置/components/Student.vue
@@ -2,6 +2,8 @@
     <div class="school">
         <h2>学生姓名：{{name}}</h2>
         <h2>学生性别：{{gender}}</h2>
+        <h2>学生年龄：{{myAge+1}}</h2>
+        <button @click="updateAge">尝试修改收到的年龄</button>
     </div>
 </template>
 
@@ -10,16 +12,41 @@ export default {
     name: 'School',
     data() {
         return {
-            name: '张三',
-            gender: '男'
-
+            msg: '我是一个学生',
+            myAge: this.age
+        }
+    },
+    methods: {
+        updateAge(){
+            this.myAge ++
         }
     },
 
+    //简单声明接收
+    // props: ['name','gender','age'], 
+    
+    //接收的同时对数据进行类型限制
+    /* props: {
+        name: String,
+        gender: String,
+        age: Number
+    } */
 
-   
-
-   
+    //接收的同时对数据进行类型限制+默认值的指定+必要性的限制
+    props: {
+        name: {
+            type: String, //name的类型是字符串
+            required: true //name是必要的
+        },
+        gender: {
+            type: String,
+            default: '男' //默认值
+        },
+        age: {
+            type: Number,
+            required: true
+        }
+    }
 }
 </script>
 
