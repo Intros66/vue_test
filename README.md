ba4efcb8d8a50b7b75b73327e555f24236531893
Intros66 <small_wn@163.com>
2021-08-03 09:34:02 +0800

    props

--- README.md
+++ README.md
@@ -26,3 +26,16 @@
     <School ref="xxx"></School>
     获取：this.$refs.xxx
     ```
+
+## 配置项props
+- 功能：让组件接收外部传过来的数据
+  - 传递数据
+    - <Demo name="xxx"/>
+  - 接收数据
+    - 第一种方式（只接收）
+      - props:['name']
+    - 第二种方式（限制类型）
+      - props:{name:String}
+    - 第三种方式（限制类型、限制必要性、指定默认值）
+      - props:{name:{type:String,required:true,defalut:'老王'}}
+- **props是只读的，Vue底层会监测你对props的修改，如果进行了修改，就会发出警告，若业务需求确实需要修改，那么请复制props的内容到data中一份，然后去修改data中的数据。**
---
+++ src/components/Student.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="school">
+        <h2>学生姓名：{{name}}</h2>
+        <h2>学生性别：{{gender}}</h2>
+        <h2>学生年龄：{{myAge+1}}</h2>
+        <button @click="updateAge">尝试修改收到的年龄</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'School',
+    data() {
+        return {
+            msg: '我是一个学生',
+            myAge: this.age
+        }
+    },
+    methods: {
+        updateAge(){
+            this.myAge ++
+        }
+    },
+
+    //简单声明接收
+    // props: ['name','gender','age'], 
+    
+    //接收的同时对数据进行类型限制
+    /* props: {
+        name: String,
+        gender: String,
+        age: Number
+    } */
+
+    //接收的同时对数据进行类型限制+默认值的指定+必要性的限制
+    props: {
+        name: {
+            type: String, //name的类型是字符串
+            required: true //name是必要的
+        },
+        gender: {
+            type: String,
+            default: '男' //默认值
+        },
+        age: {
+            type: Number,
+            required: true
+        }
+    }
+}
+</script>
+
