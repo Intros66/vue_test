70d4ae4cc95061230e0847e66c5b3b123b2bd5a1
Intros66 <small_wn@163.com>
2021-08-03 10:28:55 +0800

    plugins

---
+++ 05_src_插件/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+      <Student></Student>
+      <hr/>
+      <School></School>
+  </div>
+</template>
+
+<script>
+//引入School组件
+import Student from './components/Student'
+import School from './components/School'
+
+export default {
+    name: 'App',
+    data() {
+        return {
+            msg: '欢迎'
+        }
+    },
+     components: {
+        Student,
+        School
+    },
+    methods: {
+        showDOM(){
+            console.log(this.$refs);
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
