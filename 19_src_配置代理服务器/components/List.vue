782c1deea2ba6b0bf198c230cbba6a4edf0577ab
intros66 <small_wn@163.com>
2021-08-05 16:28:44 +0800

    代理服务器

---
+++ 19_src_配置代理服务器/components/List.vue
@@ -0,0 +1,66 @@
+<template>
+  <ul class="todo-main">
+    <transition-group name="todo" appear="">
+        <Item 
+          v-for="todoObj in todos" 
+          :key="todoObj.id" 
+          :todo="todoObj" 
+        >
+        </Item>
+    </transition-group>
+  
+  </ul>
+</template>
+
+<script>
+import Item from "./Item";
+export default {
+  name: "List",
+  props:['todos'],
+  components: {
+    Item
+  },
+  data() {
+    return {
+      
+    };
+  },
+  methods: {}
+};
+</script>
+
+<style scoped>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+
+.todo-enter-active {
+      animation: atguigu 0.5s linear;
+  }
+
+.todo-leave-active {
+    animation: aiguigu 0.5s linear reverse;
+}
+
+@keyframes atguigu {
+  from {
+      transform: translateX(-100%);
+  } 
+  to {
+        transform: translateX(0)
+  }
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,33 @@
 module.exports = {
-    lintOnSave: false
+    pages: {
+        index: {
+            //入口
+            entry: 'src/main.js'
+        }
+    },
+    //关闭语法检查
+    lintOnSave: false,
+    //开启代理服务器 方式一
+   /*  devServer: {
+        proxy: 'http://localhost:5000'
+    }, */
+
+    //开启代理服务器 方式二
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:5000',
+                ws: true,//用于支持websocket
+                changeOrigin: true,//用于控制请求头中的Host值
+                pathRewrite:{'^/api': '/'},
+            },
+            '/demo': {
+                target: 'http://localhost:5001',
+                pathRewrite:{'^/demo': '/'},
+                ws: true,//用于支持websocket
+                changeOrigin: true //用于控制请求头中的Host值
+            }
+           
+        }
+      }
 }
