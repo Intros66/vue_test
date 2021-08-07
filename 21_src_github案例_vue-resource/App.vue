aec9df785aed3c67f62d7ac8ee485e194ca94595
intros66 <small_wn@163.com>
2021-08-07 15:46:30 +0800

    vue-resourse

---
+++ 21_src_github案例_vue-resource/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <Search></Search>
+    <List></List>
+  </div>
+</template>
+
+<script>
+import Search from "./components/Search";
+import List from "./components/List";
+import axios from "axios";
+
+export default {
+  name: "App",
+  components: { Search, List },
+  methods: {}
+};
+</script>
+
+<style>
+</style>
