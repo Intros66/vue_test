d92ce4ff2e2fda07b84be13300325ee356ea7cde
intros66 <small_wn@163.com>
2021-08-08 14:06:42 +0800

    求和_纯vue版

---
+++ 23_src_求和案例_纯vue版/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Count/>
+  </div>
+</template>
+
+<script>
+import Count from "./components/Count";
+import axios from "axios";
+
+export default {
+  name: "App",
+  components: { Count },
+  methods: {}
+};
+</script>
+
+<style scoped>
+
+</style>
