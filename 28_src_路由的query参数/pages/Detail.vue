bec167efe3d9a35d21912c7d3bb35ed0f91b714e
intros66 <small_wn@163.com>
2021-08-09 15:22:28 +0800

    路由query参数

---
+++ 28_src_路由的query参数/pages/Detail.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul>
+      <li>消息编号：{{ $route.query.id }}</li>
+      <li>消息标题：{{ $route.query.title }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+    name: 'Detail',
+    data() {
+        return {
+            id: this.$route.query.id,
+            title: this.$route.query.title
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
