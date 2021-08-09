72a7d34a55d24df9e7fabf3e22d6cb7015e4dd18
intros66 <small_wn@163.com>
2021-08-09 15:43:48 +0800

    路由params参数

---
+++ 30_src_路由的params参数/pages/Detail.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul>
+      <li>消息编号：{{ $route.params.id }}</li>
+      <li>消息标题：{{ $route.params.title }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+    name: 'Detail',
+    data() {
+        return {
+            id: this.$route.query.id,
+            title: this.$route.query.title
+        }
+    },
+    mounted() {
+        console.log(this.$route);
+    },
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ 30_src_路由的params参数/pages/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>我是Home的内容</h2>
+    <ul class="nav nav-tabs">
+      <li>
+        <router-link class="list-group-item" active-class="active" to="/home/news">News</router-link>
+      </li>
+      <li>
+        <router-link class="list-group-item" active-class="active" to="/home/message">Message</router-link>
+      </li>
+    </ul>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  beforeDestroy() {
+    console.log("Home即将被销毁");
+  }
+  /*   mounted() {
+        console.log('Home挂载完毕',this);
+        window.homeRoute = this.$route
+        window.homeRouter = this.$router
+    }, */
+};
+</script>
