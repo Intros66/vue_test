a4042063254c44fadfda5ab5fa5a5789d6b543dc
intros66 <small_wn@163.com>
2021-08-09 17:39:39 +0800

    独享路由守卫

---
+++ 36_src_独享路由守卫/pages/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h2>我是Home的内容</h2>
+    <ul class="nav nav-tabs">
+      <li>
+        <router-link
+          class="list-group-item"
+          active-class="active"
+          to="/home/news"
+          >News</router-link
+        >
+      </li>
+      <li>
+        <router-link
+          class="list-group-item"
+          active-class="active"
+          to="/home/message"
+          >Message</router-link
+        >
+      </li>
+    </ul>
+    <keep-alive include="News">
+      <router-view></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  beforeDestroy() {
+    console.log("Home即将被销毁");
+  }
+  /*   mounted() {
+        console.log('Home挂载完毕',this);
+        window.homeRoute = this.$route
+        window.homeRouter = this.$router
+    }, */
+};
+</script>
